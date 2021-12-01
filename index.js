5c6ea52735fdc56096fb27c9c63793cff8e04d76
Rajasekar <Rajasekar@myunitymobiles-Mac-mini.local>
2021-12-01 22:24:57 +0530

    Day one:  - Found the increments in the Sonar reading  - Reduced the noise in sonar reading by narrowing the increments for the reading with three unit window

---
+++ index.js
@@ -0,0 +1,6 @@
+const day1_1 = require("./day-1/puzzle_one");
+const day1_2 = require("./day-1/puzzle_two");
+
+console.log("Advent of code Day 1");
+day1_1.printIncrements();
+day1_2.printSlidingIncrements();
