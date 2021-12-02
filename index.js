a285ee5631a52c2d466c45e214e51a9c87b87469
Rajasekar <rajasekar.venugopal96@gmail.com>
2021-12-02 12:47:24 +0530

    Day 2: Dive!  - Found the horizontal and depth of the ship with the directions.  - Found the horizontal and depth with the projected aim and directions.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const day1_1 = require("./day-1/puzzle_one");
 const day1_2 = require("./day-1/puzzle_two");
+const day2_1 = require("./day-2/puzzle_one");
+const day2_2 = require("./day-2/puzzle_two");
 
 console.log("Advent of code Day 1");
 day1_1.printIncrements();
 day1_2.printSlidingIncrements();
+
+console.log("\n\nAdvent of code Day 1");
+day2_1.printLocation();
+day2_2.printLocation();
