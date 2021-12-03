17decfa86c1902feb92b7ed7a394bffc49711782
Rajasekar <rajasekar.venugopal96@gmail.com>
2021-12-03 22:04:10 +0530

    Day 3: Binary Diagnostic  - Found the Power consumption of the ship.  - Found the Lifesupport Rating.

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const day1_1 = require("./day-1/puzzle_one");
 const day1_2 = require("./day-1/puzzle_two");
 const day2_1 = require("./day-2/puzzle_one");
 const day2_2 = require("./day-2/puzzle_two");
+const day3_1 = require("./day-3/puzzle_one");
+const day3_2 = require("./day-3/puzzle_two");
 
-console.log("Advent of code Day 1");
+console.log("\n\nAdvent of code Day 1");
 day1_1.printIncrements();
 day1_2.printSlidingIncrements();
 
-console.log("\n\nAdvent of code Day 1");
+console.log("\n\nAdvent of code Day 2");
 day2_1.printLocation();
 day2_2.printLocation();
+
+console.log("\n\nAdvent of code Day 3");
+day3_1.printPowerConsumption();
+day3_2.printLifeSupport();
