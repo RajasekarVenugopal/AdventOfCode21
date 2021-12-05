85e21b8a18f5e09db0578d926ab53a6e45f4c25f
Rajasekar <rajasekar.venugopal96@gmail.com>
2021-12-06 00:04:31 +0530

    Day 5: Hydrothermal Venture  - Found the vertical and horizontal intersecting lines;  - Found the intersecting lines that are 45deg diagonal

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const day3_1 = require("./day-3/puzzle_one");
 const day3_2 = require("./day-3/puzzle_two");
 const day4_1 = require("./day-4/puzzle_one");
 const day4_2 = require("./day-4/puzzle_two");
+const day5_1 = require("./day-5/puzzle_one");
 
 console.log("\n\nAdvent of code Day 1");
 day1_1.printIncrements();
@@ -22,3 +23,6 @@ day3_2.printLifeSupport();
 console.log("\n\nAdvent of code Day 4");
 day4_1.printBingoScore();
 day4_2.printLosingBingoScore();
+
+console.log("\n\nAdvent of code Day 5");
+day5_1.printIntersectionPoints();
