39efaa9d8d6d3b5bf0de2df9f1798e8539c1db60
Rajasekar <rajasekar.venugopal96@gmail.com>
2021-12-08 07:20:10 +0530

    Day 7: The Treachery of Whales  - Found the min fuel cost with mileage of 1  - Found the min fuel cost with decremental mileage

--- index.js
+++ index.js
@@ -8,25 +8,29 @@ const day4_1 = require("./day-4/puzzle_one");
 const day4_2 = require("./day-4/puzzle_two");
 const day5_1 = require("./day-5/puzzle_one_two");
 const day6_1 = require("./day-6/puzzle_one_two");
+const day7_1 = require("./day-7/puzzle_one_two");
 
-console.log("\n\nAdvent of code Day 1");
-day1_1.printIncrements();
-day1_2.printSlidingIncrements();
+// console.log("\n\nAdvent of code` Day 1");
+// day1_1.printIncrements();
+// day1_2.printSlidingIncrements();
 
-console.log("\n\nAdvent of code Day 2");
-day2_1.printLocation();
-day2_2.printLocation();
+// console.log("\n\nAdvent of code Day 2");
+// day2_1.printLocation();
+// day2_2.printLocation();
 
-console.log("\n\nAdvent of code Day 3");
-day3_1.printPowerConsumption();
-day3_2.printLifeSupport();
+// console.log("\n\nAdvent of code Day 3");
+// day3_1.printPowerConsumption();
+// day3_2.printLifeSupport();
 
-console.log("\n\nAdvent of code Day 4");
-day4_1.printBingoScore();
-day4_2.printLosingBingoScore();
+// console.log("\n\nAdvent of code Day 4");
+// day4_1.printBingoScore();
+// day4_2.printLosingBingoScore();
 
-console.log("\n\nAdvent of code Day 5");
-day5_1.printIntersectionPoints();
+// console.log("\n\nAdvent of code Day 5");
+// day5_1.printIntersectionPoints();
 
-console.log("\n\nAdvent of code Day 5");
-day6_1.printTotalNoOfLanterns();
+// console.log("\n\nAdvent of code Day 6");
+// day6_1.printTotalNoOfLanterns();`
+
+console.log("\n\nAdvent of code Day 7");
+day7_1.printTotalCost();
